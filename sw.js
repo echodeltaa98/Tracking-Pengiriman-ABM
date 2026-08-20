eeb2d40f161293415f618375123febe721a79d25
echodeltaa98 <edwindian77@gmail.com>
2026-08-20 15:27:15 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = 'tracking-abm-pwa-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  // Do not intercept the Google Apps Script app itself.
+  if (url.hostname.includes('script.google.com') ||
+      url.hostname.includes('script.googleusercontent.com')) {
+    return;
+  }
+
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
